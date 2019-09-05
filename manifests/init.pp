30485dcf1ed22028ee780cc86e0588e6939a6b54
t.jursonovics <tamas.jursonovics@telekom.de>
2019-09-05 14:25:56 +0200

    node classification is working

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,11 +7,6 @@
 class tc (
   String $rolefile = '/etc/tcrole'
 ){
-  #TODO: remove, this is just debug
-  file {'/tmp/tcworking.txt':
-    content => "module tc loaded by hydra\n"
-  }
-
   # classify node
   if ! $facts['tcrole'] {
     file {'/tmp/role.txt':
@@ -20,10 +15,8 @@ class tc (
   } elsif $facts['tcrole'] == 'trafficops' {
     include tc::to
   } else {
-    #TODO: remove, this is just debug
     file {'/tmp/role.txt':
       content => "I have an unknown role: ${facts['tcrole']}\n"
     }
   }
-
 }
