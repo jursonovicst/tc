d5f67d068edcfc468cb603e202e8cdb6fa394072
t.jursonovics <tamas.jursonovics@telekom.de>
2019-09-12 13:59:41 +0200

    Change traffic portal listen port to 8080, on 80 nodejs is already listening.

--- manifests/tp/configure.pp
+++ manifests/tp/configure.pp
@@ -11,7 +11,7 @@ class tc::tp::configure {
   File  <<| title == '/opt/traffic_portal/public/traffic_portal_properties.json' |>>{
     notify => Service['traffic_portal']
   }
-  Service{'traffic_portal':
+  Service { 'traffic_portal':
     ensure     => 'running',
     enable     => true,
     hasrestart => true,
@@ -20,14 +20,15 @@ class tc::tp::configure {
 
 
 define tc::tp::configure::register_to (
-  String $base_url = $title
+  String $base_url = $title,
+  Integer $port    = 8080,
 ) {
-  @@file{ '/etc/traffic_portal/conf/config.js':
-    ensure => 'present',
-    content => epp('tc/tp/config.js.epp', { 'base_url' => $base_url}),
+  @@file { '/etc/traffic_portal/conf/config.js':
+    ensure  => 'present',
+    content => epp('tc/tp/config.js.epp', { 'base_url' => $base_url, 'port' => $port }),
   }
-  @@file{ '/opt/traffic_portal/public/traffic_portal_properties.json':
-    ensure => present,
-    content => epp('tc/tp/traffic_portal_properties.json.epp', { 'base_url' => $base_url}),
+  @@file { '/opt/traffic_portal/public/traffic_portal_properties.json':
+    ensure  => present,
+    content => epp('tc/tp/traffic_portal_properties.json.epp', { 'base_url' => $base_url }),
   }
 }
